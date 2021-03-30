a2ca33a98ffa64d8ec09a99ef172e7b560821cf5
glenchiridza <glenchiridz@gmail.com>
2021-03-30 19:17:07 +0200

    swipe deletion fix

--- app/src/main/java/com/glencconnnect/notekeeper/MainActivity.java
+++ app/src/main/java/com/glencconnnect/notekeeper/MainActivity.java
@@ -63,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+        touchHelper.attachToRecyclerView(recyclerView);
     }
 
     @Override
